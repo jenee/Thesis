3014257d979bfe46f5a6fe691359b442e570fbe9
Jenee <jenee.hughes@gmail.com>
2012-05-02 13:25:33 -0700

    Added hyphenation dictionary
    I won't be doing anything with this now, but I like it a lot, and it
    would be super useful for splitting words over syllables. Maybe.
    
    Regardless, it's cool.

---
+++ MHHyphenationDictionary/ReadMe_files/ReadMe.js
@@ -0,0 +1,52 @@
+var smallTransparentGif = "";
+function fixupIEPNG(strImageID, transparentGif) 
+{
+    smallTransparentGif = transparentGif;
+    if (windowsInternetExplorer)
+    {
+        var img = document.getElementById(strImageID);
+        if (img)
+        {
+            var src = img.src;
+            img.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + src + "', sizingMethod='scale')";
+            img.src = transparentGif;
+            img.attachEvent("onpropertychange", imgPropertyChanged);
+        }
+    }
+}
+
+var windowsInternetExplorer = false;
+function detectBrowser()
+{
+    windowsInternetExplorer = false;
+    var appVersion = navigator.appVersion;
+    if ((appVersion.indexOf("MSIE") != -1) &&
+        (appVersion.indexOf("Macintosh") == -1))
+    {
+        windowsInternetExplorer = true;
+    }
+}
+
+var inImgPropertyChanged = false;
+function imgPropertyChanged()
+{
+    if ((window.event.propertyName == "src") && (! inImgPropertyChanged))
+    {
+        inImgPropertyChanged = true;
+        var el = window.event.srcElement;
+        if (el.src != smallTransparentGif)
+        {
+            el.filters.item(0).src = el.src;
+            el.src = smallTransparentGif;
+        }
+        inImgPropertyChanged = false;
+    }
+}
+
+function onPageLoad()
+{
+    detectBrowser();
+    fixupIEPNG("id1", "ReadMe_files/transparent.gif");
+    return true;
+}
+
