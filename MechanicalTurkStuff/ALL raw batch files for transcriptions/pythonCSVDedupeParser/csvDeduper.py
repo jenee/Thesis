f434f6e758842f4a6e0c914920cdde28c645c955
Jenee <jenee.hughes@gmail.com>
2012-08-29 16:50:02 -0700

    Currently, it doesn't output the csv properly. Going to work on that.

--- MechanicalTurkStuff/ALL raw batch files for transcriptions/pythonCSVDedupeParser/csvDeduper.py
+++ MechanicalTurkStuff/ALL raw batch files for transcriptions/pythonCSVDedupeParser/csvDeduper.py
@@ -7,7 +7,7 @@ import time
 def toPyDate(dateStr):
    #takes dates of this format: 'Fri Jun 08 09:21:50 PDT 2012'
    # format % explanations at: http://www.geezer.org/sw/mvform/doc/strftime.txt
-   print "dateStr: " + dateStr
+   #print "dateStr: " + dateStr
    timeStruct = time.strptime( dateStr, "%a %b %d %H:%M:%S %Z %Y" )
    
    ''' this version looks for the current time zone
@@ -15,7 +15,7 @@ def toPyDate(dateStr):
    '''
    epochTime = time.mktime(timeStruct) - time.timezone
 
-   print epochTime
+   #print epochTime
    return epochTime
 
 
@@ -29,15 +29,7 @@ filename = sys.argv[1]
 
 print filename
 # got this from here: http://stackoverflow.com/questions/1532810/how-to-read-lines-from-a-file-into-a-multidimensional-array-or-an-array-of-list
-array = list( csv.reader( open( filename ) ) );
-'''
-data = csv.reader(open(filename))
-# Read the column names from the first line of the file
-fields = data.next()
-
-for s in fields  :
-   print s
-'''
+array = list( csv.reader( open( filename ) ) )
 #print array[0]
 #print array[1]
 
@@ -45,19 +37,34 @@ workerIdIndex = 15 #WorkerID, column P
 taskIdIndex = 0 #hitID, column A
 submitDateIndex = 18 #submitTime column S
 
-HIT = 1
-workerArg = array[HIT][workerIdIndex]
-taskArg = array[HIT][taskIdIndex]
-submitDate = array[HIT][submitDateIndex]
+#HIT = 1
+#workerArg = array[HIT][workerIdIndex]
+#taskArg = array[HIT][taskIdIndex]
+#submitDate = array[HIT][submitDateIndex]
 
-print "worker: " +workerArg
-print "task : " +taskArg
+#print "worker: " +workerArg
+#print "task : " +taskArg
 
-epochTest = toPyDate(submitDate)
-print epochTest
+#epochTest = toPyDate(submitDate)
+#print epochTest
 print
-
-
+i = 0
+for HIT in array:
+   string = ""
+   j = 0
+   #print HIT
+   for field in HIT:
+      #string += "\""
+      if i == 0:
+         string += field;
+      elif j == submitDateIndex:
+         string += str(toPyDate(field))
+      else:
+         string += field;
+      j = j + 1
+      #string += "\", "
+   print string
+   i = i + 1
 print
 
 
