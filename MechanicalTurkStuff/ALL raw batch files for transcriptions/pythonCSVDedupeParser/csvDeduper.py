ba7be4fb22f41f2f9e2025febe38b3851d13ccf1
Jenee <jenee.hughes@gmail.com>
2012-08-28 18:58:25 -0700

    CSV parsed into 2d array...It was that easy? Well, that was ridiculous.

--- MechanicalTurkStuff/ALL raw batch files for transcriptions/pythonCSVDedupeParser/csvDeduper.py
+++ MechanicalTurkStuff/ALL raw batch files for transcriptions/pythonCSVDedupeParser/csvDeduper.py
@@ -9,13 +9,19 @@ filename = sys.argv[1]
 
 print filename
 
-
+array = list( csv.reader( open( filename ) ) );
+'''
 data = csv.reader(open(filename))
 # Read the column names from the first line of the file
 fields = data.next()
 
 for s in fields  :
    print s
+'''
+print array[0]
+print array[1]
 
+
+print
 
 
