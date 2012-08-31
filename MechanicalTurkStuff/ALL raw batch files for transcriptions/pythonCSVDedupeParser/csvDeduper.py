946c4a02ecec0cdbe9d49595392b158023e270f7
Jenee <jenee.hughes@gmail.com>
2012-08-30 20:12:07 -0700

    Deduper done but untested, have a unique list of urls

--- MechanicalTurkStuff/ALL raw batch files for transcriptions/pythonCSVDedupeParser/csvDeduper.py
+++ MechanicalTurkStuff/ALL raw batch files for transcriptions/pythonCSVDedupeParser/csvDeduper.py
@@ -1,25 +1,5 @@
 import sys
 import csv
-import time
-
-#convert date format strings to python dates
-# http://docs.python.org/library/time.html#time.strptime
-def toPyDate(dateStr):
-   #takes dates of this format: 'Fri Jun 08 09:21:50 PDT 2012'
-   # format % explanations at: http://www.geezer.org/sw/mvform/doc/strftime.txt
-   #print "dateStr: " + dateStr
-   timeStruct = time.strptime( dateStr, "%a %b %d %H:%M:%S %Z %Y" )
-   
-   ''' this version looks for the current time zone
-   timeStruct = time.strptime( dateStr, "%a %b %d %I:%M:%S %Z %Y" )
-   '''
-   epochTime = time.mktime(timeStruct) - time.timezone
-
-   #print epochTime
-   return epochTime
-
-
-
 
 if len(sys.argv) < 2:
    sys.stderr.write('Usage: sys.argv[0] ')
@@ -27,42 +7,73 @@ if len(sys.argv) < 2:
 
 filename = sys.argv[1]
 
-#print filename
 # got this from here: http://stackoverflow.com/questions/1532810/how-to-read-lines-from-a-file-into-a-multidimensional-array-or-an-array-of-list
-array = list( csv.reader( open( filename, "rU") ) )
-#print array[0]
-#print array[1]
+origHitList = list( csv.reader( open( filename, "rU") ) )
 
-workerIdIndex = 15 #WorkerID, column P
-taskIdIndex = 0 #hitID, column A
-submitDateIndex = 18 #submitTime column S
+dedupedTaskList = []
 
-#HIT = 1
-#workerArg = array[HIT][workerIdIndex]
-#taskArg = array[HIT][taskIdIndex]
-submitDate = array[1][submitDateIndex]
+uniqueURLlist = buildUniqueURLlist()
 
-#print "worker: " +workerArg
-#print "task : " +taskArg
+workerIdIndex = 15 		#WorkerID, column P
+urlIndex = 27 			#input.audioURL, column AB
+taskIdIndex = 0 		#hitID, column A
+submitDateIndex = 18 	#submitTime column S
+statusIndex = 16		#AssignmentStatus, column Q
+answerIndex = 28 		#Answer.answer, column AC
+countryIndex = 29 		#Answer.country, column AD
 
-epochTest = toPyDate(submitDate)
-print epochTest
-print
-i = 0
-for HIT in array:
-   string = ""
-   j = 0
-   for field in HIT:
-      if i == 0:
-         string += field;
-      elif j == submitDateIndex:
-         string += str(toPyDate(field))
-      else:
-         string += field;
-      j = j + 1
-      string += ", "
-   print string
-   i = i + 1
-print
 
+for urlString in uniqueURLlist
+
+	urlTaskList = allTasksWithURL(origHitList, urlString)
+	
+	#set default hash entry to a blank list 
+	workerCountDict = defaultdict(list)
+
+	for HIT in urlTaskList
+		workerCountDict[ HIT[workerIDIndex] ].append(HIT) 
+
+	for workerHITList in workerCountDict
+		dedupedTaskList.add( getEarliestHit (workerHITList) )
+
+for HIT in dedupedTaskList:
+	if HIT[statusIndex] != "Rejected" and
+		HIT[answerIndex].strip() != "" and
+		HIT[countryIndex].strip() != "" : 
+		print HIT
+
+def buildUniqueURLlist():
+	uniqueURLlist = []
+	uniqueURLlist.append(" http://ani.pe/jenee/A.20.163.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.19.49.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.19.283.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.19.139.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.19.137.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.19.133.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.19.116.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.18.68.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.18.65.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.18.284.mp3")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.18.140.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.18.140.mp3")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.18.125.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.17.69.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.17.51.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.17.141.wav")
+	uniqueURLlist.append(" http://ani.pe/jenee/A.17.135.wav")
+	return uniqueURLlist
+			 
+
+def allTasksWithURL(origHitList, urlString):
+	urlList = []
+	for HIT in origHitList:
+		if HIT[urlIndex] == urlString:
+			urlList.append( HIT )
+	return urlList
+			
+
+def getEarliestHit( workerHitList ):
+	sortedHitList = sorted( workerHitList, key=lamba HIT: HIT[ submitDateIndex ] ) 
+	return sortedHitList[0]
+		
 
