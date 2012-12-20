3dd021176fdf01d03b32457f2d6b1e1e26a5ec29
Jenee <jenee.hughes@gmail.com>
2012-12-20 14:25:11 -0800

    DEAR LORD IN HEAVEN Protovis is amazing!
    The most important files in this commit:
    
    These show a *sunburst diagram* of the frequency of "iced ink" oronyms! :D
    
    I'm using a MULTIPLICATIVE frequency metric, since that's more correct anyway.
    
    TODO:
    
    (1) Revise all frequency metrics to be products instead of sums.
    (2) Make protovis of an ice cold hour oronyms!  May want to write a script to generate that.

---
+++ Visualization/mbostock-protovis-1a61bac/tests/icedInkFreqMetric.js
@@ -0,0 +1,17 @@
+var icedInkRoot = {
+  I: {
+    stink: 12859610000
+  },
+  aye: {
+    stink: 168870882
+  },
+  eye: {
+    stink: 34614500
+  },
+  ay: {
+    stink: 8583102
+  },
+  iced: {
+    ink: 1040778
+  }
+};
