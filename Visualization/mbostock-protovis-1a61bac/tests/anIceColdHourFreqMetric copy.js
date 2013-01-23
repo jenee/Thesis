6478d083dc180426a52f5933737752aa1dfc5102
Jenee <jenee.hughes@gmail.com>
2013-01-23 00:22:19 -0800

    Added a bunch of stuff, made Observed sunburst
    But now, for some reason, the predictive sunburst doesn't match.
    
    Also, the colors are off, because I added another category,
    unpredicted, to the observed sunburst. I'll have to figure out what's
    wrong with the predicted sunburst protovis stuff, and then add
    that unpredicted category, so that the colors will match on my observed
    and predicted sunbursts
    
    Also, I added a bunch of figures for transcriptions per recording.
    I need to go through those.
    
    Also, a bunch of other stuff.
    
    When I'm not working on the command line, I always forget to
    commit regularly!

---
+++ Visualization/mbostock-protovis-1a61bac/tests/anIceColdHourFreqMetric copy.js
@@ -0,0 +1,526 @@
+var anIceColdHourRoot = { 
+   n: {
+      ay: {
+         scold: {
+            hour: 78693,
+
+            our: 480303,
+
+         },
+         skol: {
+            dour: 6787,
+
+            dower: 6760,
+
+         }
+      },
+      aye: {
+         scold: {
+            hour: 202563,
+
+            our: 604173,
+
+         },
+         skol: {
+            dour: 130657,
+
+            dower: 130630,
+
+         }
+      },
+      eye: {
+         scold: {
+            hour: 98810,
+
+            our: 500420,
+
+         },
+         skol: {
+            dour: 26904,
+
+            dower: 26877,
+
+         }
+      },
+      i: {
+         scold: {
+            hour: 10009937,
+
+            our: 10411547,
+
+         },
+         skol: {
+            dour: 9938031,
+
+            dower: 9938004,
+
+         }
+      },
+      ice: {
+         coal: {
+            dour: 32799,
+
+            dower: 32772,
+
+         },
+         cold: {
+            hour: 136889,
+
+            our: 538499,
+
+         },
+         cole: {
+            dour: 14424,
+
+            dower: 14397,
+
+         },
+         kohl: {
+            dour: 13263,
+
+            dower: 13236,
+
+         }
+      },
+      icecold: {
+         hour: 71892,
+
+         our: 473502,
+
+      }
+   },
+   a: {
+      gneiss: {
+         coal: {
+            dour: 7556885,
+
+            dower: 7556858,
+
+         },
+         cold: {
+            hour: 7660975,
+
+            our: 8062585,
+
+         },
+         cole: {
+            dour: 7538510,
+
+            dower: 7538483,
+
+         },
+         kohl: {
+            dour: 7537349,
+
+            dower: 7537322,
+
+         }
+      },
+      nice: {
+         coal: {
+            dour: 7747572,
+
+            dower: 7747545,
+
+         },
+         cold: {
+            hour: 7851662,
+
+            our: 8253272,
+
+         },
+         cole: {
+            dour: 7729197,
+
+            dower: 7729170,
+
+         },
+         kohl: {
+            dour: 7728036,
+
+            dower: 7728009,
+
+         }
+      },
+      nigh: {
+         scold: {
+            hour: 7609721,
+
+            our: 8011331,
+
+         },
+         skol: {
+            dour: 7537815,
+
+            dower: 7537788,
+
+         }
+      },
+      nye: {
+         scold: {
+            hour: 7608364,
+
+            our: 8009974,
+
+         },
+         skol: {
+            dour: 7536458,
+
+            dower: 7536431,
+
+         }
+      }
+   },
+   an: {
+      ay: {
+         scold: {
+            hour: 872832,
+
+            our: 1274442,
+
+         },
+         skol: {
+            dour: 800926,
+
+            dower: 800899,
+
+         }
+      },
+      aye: {
+         scold: {
+            hour: 996702,
+
+            our: 1398312,
+
+         },
+         skol: {
+            dour: 924796,
+
+            dower: 924769,
+
+         }
+      },
+      eye: {
+         scold: {
+            hour: 892949,
+
+            our: 1294559,
+
+         },
+         skol: {
+            dour: 821043,
+
+            dower: 821016,
+
+         }
+      },
+      i: {
+         scold: {
+            hour: 10804076,
+
+            our: 11205686,
+
+         },
+         skol: {
+            dour: 10732170,
+
+            dower: 10732143,
+
+         }
+      },
+      ice: {
+         coal: {
+            dour: 826938,
+
+            dower: 826911,
+
+         },
+         cold: {
+            hour: 931028,
+
+            our: 1332638,
+
+         },
+         cole: {
+            dour: 808563,
+
+            dower: 808536,
+
+         },
+         kohl: {
+            dour: 807402,
+
+            dower: 807375,
+
+         }
+      },
+      icecold: {
+         hour: 866031,
+
+         our: 1267641,
+
+      }
+   },
+   ann: {
+      ay: {
+         scold: {
+            hour: 96018,
+
+            our: 497628,
+
+         },
+         skol: {
+            dour: 24112,
+
+            dower: 24085,
+
+         }
+      },
+      aye: {
+         scold: {
+            hour: 219888,
+
+            our: 621498,
+
+         },
+         skol: {
+            dour: 147982,
+
+            dower: 147955,
+
+         }
+      },
+      eye: {
+         scold: {
+            hour: 116135,
+
+            our: 517745,
+
+         },
+         skol: {
+            dour: 44229,
+
+            dower: 44202,
+
+         }
+      },
+      i: {
+         scold: {
+            hour: 10027262,
+
+            our: 10428872,
+
+         },
+         skol: {
+            dour: 9955356,
+
+            dower: 9955329,
+
+         }
+      },
+      ice: {
+         coal: {
+            dour: 50124,
+
+            dower: 50097,
+
+         },
+         cold: {
+            hour: 154214,
+
+            our: 555824,
+
+         },
+         cole: {
+            dour: 31749,
+
+            dower: 31722,
+
+         },
+         kohl: {
+            dour: 30588,
+
+            dower: 30561,
+
+         }
+      },
+      icecold: {
+         hour: 89217,
+
+         our: 490827,
+
+      }
+   },
+   anne: {
+      ay: {
+         scold: {
+            hour: 89469,
+
+            our: 491079,
+
+         },
+         skol: {
+            dour: 17563,
+
+            dower: 17536,
+
+         }
+      },
+      aye: {
+         scold: {
+            hour: 213339,
+
+            our: 614949,
+
+         },
+         skol: {
+            dour: 141433,
+
+            dower: 141406,
+
+         }
+      },
+      eye: {
+         scold: {
+            hour: 109586,
+
+            our: 511196,
+
+         },
+         skol: {
+            dour: 37680,
+
+            dower: 37653,
+
+         }
+      },
+      i: {
+         scold: {
+            hour: 10020713,
+
+            our: 10422323,
+
+         },
+         skol: {
+            dour: 9948807,
+
+            dower: 9948780,
+
+         }
+      },
+      ice: {
+         coal: {
+            dour: 43575,
+
+            dower: 43548,
+
+         },
+         cold: {
+            hour: 147665,
+
+            our: 549275,
+
+         },
+         cole: {
+            dour: 25200,
+
+            dower: 25173,
+
+         },
+         kohl: {
+            dour: 24039,
+
+            dower: 24012,
+
+         }
+      },
+      icecold: {
+         hour: 82668,
+
+         our: 484278,
+
+      }
+   },
+   o: {
+      gneiss: {
+         coal: {
+            dour: 21051,
+
+            dower: 21024,
+
+         },
+         cold: {
+            hour: 125141,
+
+            our: 526751,
+
+         },
+         cole: {
+            dour: 2676,
+
+            dower: 2649,
+
+         },
+         kohl: {
+            dour: 1515,
+
+            dower: 1488,
+
+         }
+      },
+      nice: {
+         coal: {
+            dour: 211738,
+
+            dower: 211711,
+
+         },
+         cold: {
+            hour: 315828,
+
+            our: 717438,
+
+         },
+         cole: {
+            dour: 193363,
+
+            dower: 193336,
+
+         },
+         kohl: {
+            dour: 192202,
+
+            dower: 192175,
+
+         }
+      },
+      nigh: {
+         scold: {
+            hour: 73887,
+
+            our: 475497,
+
+         },
+         skol: {
+            dour: 1981,
+
+            dower: 1954,
+
+         }
+      },
+      nye: {
+         scold: {
+            hour: 72530,
+
+            our: 474140,
+
+         },
+         skol: {
+            dour: 624,
+
+            dower: 597,
+
+         }
+      }
+   }
+};
---
+++ Visualization/mbostock-protovis-1a61bac/tests/anIceColdHourObserved.js
@@ -0,0 +1,533 @@
+var anIceColdHourRoot = { 
+   unpredicted: {
+      o: {
+         o: {
+            o: 350
+         }
+      }
+   },
+   n: {
+      ay: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      aye: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      eye: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      i: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      ice: {
+         coal: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         cold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         cole: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         kohl: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      icecold: {
+         hour: 0,
+
+         our: 0,
+
+      }
+   },
+   a: {
+      gneiss: {
+         coal: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         cold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         cole: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         kohl: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      nice: {
+         coal: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         cold: {
+            hour: 217,
+
+            our: 3,
+
+         },
+         cole: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         kohl: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      nigh: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      nye: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      }
+   },
+   an: {
+      ay: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      aye: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      eye: {
+         scold: {
+            hour: 13,
+
+            our: 1,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      i: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      ice: {
+         coal: {
+            dour: 3,
+
+            dower: 6,
+
+         },
+         cold: {
+            hour: 352,
+
+            our: 4,
+
+         },
+         cole: {
+            dour: 0,
+
+            dower: 2,
+
+         },
+         kohl: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      icecold: {
+         hour: 2,
+
+         our: 0,
+
+      }
+   },
+   ann: {
+      ay: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      aye: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      eye: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      i: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      ice: {
+         coal: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         cold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         cole: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         kohl: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      icecold: {
+         hour: 0,
+
+         our: 0,
+
+      }
+   },
+   anne: {
+      ay: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      aye: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      eye: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      i: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      ice: {
+         coal: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         cold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         cole: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         kohl: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      icecold: {
+         hour: 0,
+
+         our: 0,
+
+      }
+   },
+   o: {
+      gneiss: {
+         coal: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         cold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         cole: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         kohl: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      nice: {
+         coal: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         cold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         cole: {
+            dour: 0,
+
+            dower: 0,
+
+         },
+         kohl: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      nigh: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      },
+      nye: {
+         scold: {
+            hour: 0,
+
+            our: 0,
+
+         },
+         skol: {
+            dour: 0,
+
+            dower: 0,
+
+         }
+      }
+   }
+};
