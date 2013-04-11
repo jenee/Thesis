51d2e3816f87acab95c1d763c3154db6a2a3d55a
Jenee <jenee.hughes@gmail.com>
2013-04-11 00:01:51 -0700

    Final draft imminent.
    Gotta: do a quick readthrough in the morning
    Redo the blue bars that aren't there (just remove them from the graphs)

---
+++ Visualization/mbostock-protovis-1a61bac/tests/protoVisDemoburst.js
@@ -0,0 +1,17 @@
+var oronymRoot = { 
+   Child1: {
+      Child1A: {
+         Child1Ai: 2,
+         Child1Aii: 3
+      },
+      Child1B: {
+         Child1Bi: 6
+      }
+   },
+   Child2: {
+      Child2A: {
+         Child2Ai: 2,
+         Child2Aii: 9
+      }
+   }
+};
